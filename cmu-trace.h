94d28ab28aa54a496784bdc9f704cf5dc5efcf04
MartMbithi <martdevelopers254@gmail.com>
2021-02-22 11:12:00 +0300

    Minor Code Formatting

--- cmu-trace.h
+++ cmu-trace.h
@@ -1,41 +1,3 @@
-/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*-
- *
- * Copyright (c) 1997 Regents of the University of California.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the Computer Systems
- *	Engineering Group at Lawrence Berkeley Laboratory.
- * 4. Neither the name of the University nor of the Laboratory may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * $Header: /cvsroot/nsnam/ns-2/trace/cmu-trace.h,v 1.29 2010/05/09 22:28:41 tom_henderson Exp $
- */
-
-/* Ported from CMU/Monarch's code, nov'98 -Padma.*/
-
 #ifndef __cmu_trace__
 #define __cmu_trace__
 
@@ -44,21 +6,15 @@
 
 #ifndef __PRETTY_FUNCTION__
 #define __PRETTY_FUNCTION__ ("")
-#endif /* !__PRETTY_FUNCTION__ */
+#endif 
 
-/* ======================================================================
-   Global Defines
-   ====================================================================== */
+/* Globalization */
 #define	DROP            'D'
 #define	RECV            'r'
 #define	SEND    	's'
 #define	FWRD    	'f'
 
-// change wrt Mike's code
 #define EOT             'x'
-
-
-
 #define TR_ROUTER	0x01
 #define TR_MAC		0x02
 #define TR_IFQ		0x04
@@ -128,12 +84,10 @@ private:
         MobileNode *node_;
 	int     newtrace_;
 
-	//<zheng: ns 2.27 removed the following part, but we need it to control the broadcast radius>
         static double  bradius;
         static double  radius_scaling_factor_;
         static double  duration_scaling_factor_;
         static void calculate_broadcast_parameters();
-	//</zheng>
 
         int initialized() { return node_ && 1; }
 	int node_energy();
@@ -168,4 +122,4 @@ private:
 
 };
 
-#endif /* __cmu_trace__ */
+#endif 
